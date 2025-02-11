aeaf68eed493066f3ff53c9ffb3f33e0df19d216
Delaw45 <ogheneovodelaw@gmail.com>
2025-02-11 10:30:34 +0100

    Initial commit

---
+++ band_name.py
@@ -0,0 +1,12 @@
+# Band name generator
+# This script generates random band names by combining inputs the users.
+# Useful for brainstorming creative names
+
+print("Welcome to the Band Name Generator.")
+
+# Get user input
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet's name?\n")
+
+# Show the generated output
+print("The name of your new band is "+ city +" "+ pet_name)
